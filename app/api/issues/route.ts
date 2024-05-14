3a9c798976ce8b804d6d1a76acc916bf3b9334c4
NidhiNayak08 <2021.nidhi.nayak@ves.ac.in>
2024-05-14 13:38:46 +0530

    build api for create issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -5,7 +5,7 @@ import prisma from "@/prisma/client";
 const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
   description: z.string().min(1),
-}); //define shaoe of obj and body of request
+}); //define shape of obj and body of request
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
